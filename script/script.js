4f030156d6b5dd5f2cf0fe37bccd239dfeadcd45
mjnaveen96 <31090107+mjnaveen96@users.noreply.github.com>
2018-09-25 17:51:11 +0530

    last

--- script/script.js
+++ script/script.js
@@ -1,13 +1,23 @@
-var arr = [];
- var arr1 =[];
+
+/*
+
+  *   @Desc   : Todo list
+  *   @Author : Naveen Jose
+
+*/
+
+
+
+var arr    =  [];
+var arr1   =  [];
 
 window.addEventListener("load",on_Load);
 
 //create node function
 
 function createNode(val) {
-  this.valu = val;
-  this.status = false;
+  this.valu   =  val;
+  this.status =  false;
   }
 
 //create a new new_element
@@ -15,7 +25,7 @@ function createNode(val) {
   function createElementFun() {
     if (document.getElementById('text').value!="")
     {
-      var inputtext = document.getElementById('text').value;
+      var inputtext  =  document.getElementById('text').value;
       document.getElementById('text').value = "";
       arr.push(new createNode(inputtext));
       save();
@@ -27,25 +37,25 @@ function createNode(val) {
     }
  }
 function save() {
-  var myJson = JSON.stringify(arr);
+  var myJson  =  JSON.stringify(arr);
   localStorage.setItem("server", myJson);
 }
 function getValues() {
-  var output = localStorage.getItem("server");
-  arr1 = JSON.parse(output);
+  var output  =  localStorage.getItem("server");
+  arr1        =  JSON.parse(output);
 }
 
 //function for loading the data from local server when page loads
 
 function on_Load() {
   getValues();
-  if(arr1 != null) {
-    var no = arr1.length;
-    for (var i = 0; i < no; i++) {
-      arr[i] = arr1[i];
+  if(arr1!=null) {
+    var no  =  arr1.length;
+    for (var i=0; i < no; i++) {
+      arr[i]  =  arr1[i];
     }
     document.getElementById("new_element").innerHTML = "";
-    for (var i = 0; i < no; i++) {
+    for (var i=0; i < no; i++) {
       show(arr1[i], i);
     }
   }
@@ -54,17 +64,17 @@ function on_Load() {
 // show values in the local storage
 
 function show(intext, id) {
-  var c = "";
+  var c  =  "";
   if(intext.status === true) {
     c = " checked";
   } else {
     c = "";
   }
-  var inputtext = intext.valu;
-  var para = document.createElement("div");
+  var inputtext  =  intext.valu;
+  var para       =  document.createElement("div");
   para.setAttribute("class", "outer");
-  para.innerHTML = "<div class = 'each_node'><span class='"+c+"' id='"+id+"'>"+inputtext+"</span><div class = 'tick'><span onclick = 'check("+id+")'>✔</span></div><div class = 'close'><span onclick = 'remove("+id+")'>✖</span></div></div>";
-  var element = document.getElementById("new_element");
+  para.innerHTML =  "<div class = 'each_node'><span class='"+c+"' id='"+id+"'>"+inputtext+"</span><div class = 'tick'><span onclick = 'check("+id+")'>✔</span></div><div class = 'close'><span onclick = 'remove("+id+")'>✖</span></div></div>";
+  var element    =  document.getElementById("new_element");
   element.append(para);
 }
 
@@ -80,9 +90,9 @@ function remove(t) {
 
 function check(a) {
   if(arr1[a].status === false) {
-    arr1[a].status = true;
+    arr1[a].status  =  true;
   } else {
-    arr1[a].status = false;
+    arr1[a].status  =  false;
   }
   save();
   on_Load();
@@ -93,12 +103,12 @@ function check(a) {
 
 function completed() {
   document.getElementById("new_element").innerHTML = "";
-  for (var i = 0; i < arr1.length; i++) {
+  for (var i=0; i < arr1.length; i++) {
     if (arr1[i].status === true) {
      show(arr1[i],i);
     }
   }
-  onclick = "on_Load()";
+  onclick  =  "on_Load()";
 }
 
 //notcomplted button
